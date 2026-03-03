4ccab0c2357e5c2fba2452233a8857cf0e81bd2c
Jules <jules.duarte@edu.esiee-it.fr>
2026-03-03 15:47:41 +0100

    checkpoint5

--- mfe-lobby/src/components/Lobby.jsx
+++ mfe-lobby/src/components/Lobby.jsx
@@ -11,7 +11,16 @@ const GAMES = [
 
 function GameCard({ game }) {
   const handleJoinGame = () => {
-    eventBus.emit('game:joined', game);
+    // TODO 3: Emettre l'evenement 'game:joined' avec le nom de la partie
+    //
+    // Indice: eventBus.emit('nomEvenement', { cle: valeur })
+    //
+    // L'evenement doit contenir: { gameName: game.name }
+    //
+    // Ecrivez votre code ici:
+
+
+    // Feedback visuel (gardez cette ligne)
     alert(`Vous avez rejoint : ${game.name}`);
   };
 
@@ -47,6 +56,9 @@ function Lobby() {
         ))}
       </div>
 
+      <div className="lobby-hint">
+        <p>Cliquez sur "Rejoindre" pour envoyer un evenement au Header !</p>
+      </div>
     </div>
   );
 }
