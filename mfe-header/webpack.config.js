7b1ce52337b420574e368e488b34d0876366e035
Jules <jules.duarte@edu.esiee-it.fr>
2026-03-03 12:11:52 +0100

    checkpoint 3

--- mfe-header/webpack.config.js
+++ mfe-header/webpack.config.js
@@ -7,11 +7,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
-    publicPath: 'http://localhost:3000/',
+    publicPath: 'http://localhost:3001/',
     clean: true,
   },
   devServer: {
-    port: 3000,
+    port: 3001,
     hot: true,
     historyApiFallback: true,
     headers: {
@@ -41,12 +41,10 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'shell',
-      remotes: {},
-      shared: {
-        react: { singleton: true, requiredVersion: '^18.2.0' },
-        'react-dom': { singleton: true, requiredVersion: '^18.2.0' },
-      },
+      name: 'mfeHeader',
+      filename: 'remoteEntry.js',
+      exposes: { './Navbar': './src/components/Navbar' },
+      shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
